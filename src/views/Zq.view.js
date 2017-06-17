7edc8271b2a50321726a76fb6a2ec8678db25ccf
ZQ <850307035@qq.com>
2017-06-17 15:52:14 +0800

    change pc details

--- src/views/Zq.view.js
+++ src/views/Zq.view.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ajaxExpanding } from '../libs/ajaxExpand.mini.min.js';
-import { config , getImgURL } from '../libs/ajax.public';
+import { config , getImgURL , getScreenSize } from '../libs/ajax.public';
 // 中间部分
 ajaxExpanding.init({
   name:'getDetails',
@@ -63,7 +63,7 @@ class Page_body extends React.Component {
   constructor(props) {
     super(props);
     // this.create_body=this.create_body.bind(this);
-
+    this.isMobile=getScreenSize().width<768;
     this.channel_name='';
     this.state={};
   }
@@ -83,19 +83,41 @@ class Page_body extends React.Component {
       if(this.props.nodata) {
         return <div id="detail_error">{this.props.error}</div>
       } else {
-        return <div id="details">
-          {this.props.videos.map((e,index)=><div className="detail_item" key={index}>
-            <img src={getImgURL(e.img)} alt={e.title}/>
-            <span  className="fontSizeS" title={e.title}>{this.words_limit(e.short_title)}</span>
-          </div>)}
-        </div>
+        if(this.isMobile) { //phone
+          return <div id="details">
+            {this.props.videos.map((e,index)=><div className="detail_item" key={index} style={{height:'340px'}}>
+              <img src={getImgURL(e.img)} alt={e.title}/>
+              <span  className="fontSizeS" title={e.title}>{this.words_limit(e.short_title)}</span>
+            </div>)}
+          </div>
+        } else { //pc
+          return <div id="pc_details">
+            {this.props.videos.map((e,index)=><div className="detail_item" key={index}>
+              <div>
+                {e.is_vip=='1'?<img src="../resource/images/vip.png" className="pc_vip" />:''}
+                <img src={getImgURL(e.img)} alt={e.title} className='pc_pic'/>
+                <div className="pc_msg" style={{
+                  width:'5.197rem',
+                  height:'3.898rem',
+                  position:'absolute',
+                  top:'0.15rem',
+                  // display:'none',
+                  background:'rgba(150,150,150,0.6)'
+                }}>
+                  <span>播放量：{e.play_count_text}</span>
+                </div>
+              </div>
+              <span  className="fontSizeS" title={e.title}>{this.words_limit(e.short_title)}</span>
+            </div>)}
+          </div>
+        }
       }
     }
   }
 
   render() {
     // this.get_msg(this.props.Title);
-    if(config.isMobile) {
+    if(this.isMobile) {
       console.log("手机端");
     } else {
       console.log("PC端");
